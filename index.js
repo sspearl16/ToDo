ebee2d3c256105da12ec43cbcd0565893b1b1221
Su Su Pearl <110281829+sspearl16@users.noreply.github.com>
2022-11-28 22:12:40 +0630

    update index.js
    added function to add an item to todo list and remove an item from todo list

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function App(){
     const [todos, setTodos] = React.useState([
             {
-                text: "learn reacct",
+                text: "learn react",
                 isCompleted: false,
             },
             {
@@ -14,9 +14,35 @@ function App(){
             }
         
     ]);
+    //code to add new todo item
+    const [value, setValue] = React.useState('');
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!value) return;
+        const newTodos = [...todos, {text:value, isCompleted:false}];
+        setTodos(newTodos);
+        setValue('');
+    }
+    //remove item from todo list
+    const removeTodo = e => {
+        const index = Number(e.target.id);
+        let temp = [...todos];
+        temp.splice(index,1); //remove the item from the index
+        setTodos(temp);
+    }
+
+    //update the state + remove item from todo
     return(<>
         {todos.map((todo,i) => 
-        <div className="todo" key={i}>{todo.text}</div>)}
+        <div className="todo" key={i} id={i} onClick={removeTodo}>{todo.text}</div>)}
+        <form onSubmit={handleSubmit}>
+            <input type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo ..."
+            onChange={e => setValue(e.target.value)}
+            />
+        </form>
     </>);
 }
 
